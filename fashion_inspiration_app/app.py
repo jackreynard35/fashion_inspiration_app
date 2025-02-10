ceb548b0e8173bf110cdc451ce6262a89045ee27
Jack Reynard <jackreynard.35@gmail.com>
2025-02-10 10:35:47 -0500

    Initial commit

---
+++ fashion_inspiration_app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = '46580532-707adb1ad10f1c143f21a46c8' 
+2
+def fetch_images(query):
+    url = f'https://pixabay.com/api/?key={API_KEY}&q={query}&image_type=photo&pretty=true'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        images = [item['webformatURL'] for item in data['hits']] 
+        return images
+    else:
+        return []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        color = request.form['color']
+        style = request.form['style']
+        occasion = request.form['occasion']
+        search_query = f"{style} {occasion} fashion outfit {color}"
+        images = fetch_images(search_query)
+
+        return render_template('suggestions.html', images=images, suggestions=search_query)
+    
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
